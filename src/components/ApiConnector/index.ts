fa5dd834092009fe4ffcec84e31424fd96df8334
Rob Friesel <rob@founddrama.net>
2020-11-22 08:33:23 -0800

    trying to get the tests working bore fruit but still tests aren't working

--- src/components/ApiConnector/index.ts
+++ src/components/ApiConnector/index.ts
@@ -1,36 +1,28 @@
-import Axios from "axios";
+import axios from "axios";
 import { DevDiceApiResponse, DevTech } from "../../types";
 
-export default class ApiConnector {
-  API_HOST: string;
+const getApiHost = (): string => {
+  const { NODE_ENV } = process.env;
 
-  constructor() {
-    this.API_HOST = this.generateApiHost();
+  if (NODE_ENV === 'development') {
+    const {
+      REACT_APP_PROTOCOL,
+      REACT_APP_HOSTNAME,
+      REACT_APP_SERVER_APP_PORT,
+    } = process.env;
+    return `${REACT_APP_PROTOCOL}//${REACT_APP_HOSTNAME}:${REACT_APP_SERVER_APP_PORT}`;
+  } else {
+    return window.location.origin;
+  }
+};
+
+export const getDieRoll = async (devTech?: DevTech): Promise<DevDiceApiResponse | void> => {
+  try {
+    const { data } = await axios.get(`${getApiHost()}/api/roll${devTech ? `/${devTech}` : ''}`);
+    return data;
+  } catch (error) {
+    console.log(error);
   }
 
-  generateApiHost = (): string => {
-    const { NODE_ENV } = process.env;
-
-    if (NODE_ENV === 'development') {
-      const {
-        REACT_APP_PROTOCOL,
-        REACT_APP_HOSTNAME,
-        REACT_APP_SERVER_APP_PORT,
-      } = process.env;
-      return `${REACT_APP_PROTOCOL}//${REACT_APP_HOSTNAME}:${REACT_APP_SERVER_APP_PORT}`;
-    } else {
-      return window.location.origin;
-    }
-  }
-
-  getDieRoll = async (devTech?: DevTech): Promise<DevDiceApiResponse | void> => {
-    try {
-      const { data } = await Axios.get(`${this.API_HOST}/api/roll${devTech ? `/${devTech}` : ''}`);
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-
-    return;
-  }
-}
+  return;
+};
