1c1e63a7c188a9319fc3fa9a84b1b448c8b6e02a
Rob Friesel <rob@founddrama.net>
2020-11-23 08:01:57 -0800

    tidier Mongo connector

--- server/dice-utils.js
+++ server/dice-utils.js
@@ -7,15 +7,16 @@ const MONGO_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@cluster0.3dnfr.
 function getDataFromMongo(collectionName, techDie) {
   const mongoClient = new MongoClient(MONGO_URI, { useUnifiedTopology: true });
   mongoClient.connect((err, client) => {
-    if (!err) {
-      console.log(`Connected to devdice; loading ${collectionName}...`);
-      const collection = client.db(MONGODB_NAME).collection(collectionName);
-      collection.find().toArray((_err, items) => {
-        techDie.options = items.map((item) => item.name);
-      });
-    } else {
-      console.error(`Error(s) connecting to dev dive :(\n${err.message}`);
+    if (err) {
+      console.error(`Error(s) connecting to devdice :(\n${err.message}`);
+      return;
     }
+
+    console.log(`Connected to devdice; loading ${collectionName}...`);
+    const collection = client.db(MONGODB_NAME).collection(collectionName);
+    collection.find().toArray((_err, items) => {
+      techDie.options = items.map((item) => item.name);
+    });
   });
 }
 
